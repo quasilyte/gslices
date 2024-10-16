03ac38053403414a015c0d8fe7c812835c416365
Iskander Sharipov <quasilyte@gmail.com>
2024-10-16 15:00:17 +0400

    add Transfer func

--- utils.go
+++ utils.go
@@ -7,3 +7,9 @@ func Swap[T any](s []T, i, j int) {
 func Clone[T any](s []T) []T {
 	return append(s[:0:0], s...)
 }
+
+func Transfer[T comparable](from, to *[]T, x T) {
+	i := Index(*from, x)
+	DeleteAt(*from, i)
+	*to = append(*to, x)
+}
