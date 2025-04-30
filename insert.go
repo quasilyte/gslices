9145ebf14252dd10328a64839353560f3cda4b29
Iskander Sharipov <quasilyte@gmail.com>
2025-04-30 18:20:42 +0400

    add InsertSliceAt

--- insert.go
+++ insert.go
@@ -3,3 +3,10 @@ package gslices
 func InsertAt[T any](s []T, x T, i int) []T {
 	return append(s[:i], append([]T{x}, s[i:]...)...)
 }
+
+func InsertSliceAt[T any](s []T, s2 []T, i int) []T {
+	result := s[:i]
+	result = append(result, s2...)
+	result = append(result, s[i:]...)
+	return result
+}
