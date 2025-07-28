a74ed95910abc35462e35eff6dab7423c3aa51bb
Iskander Sharipov <quasilyte@gmail.com>
2025-07-28 18:39:44 +0400

    make filter accept a ptr

--- filter.go
+++ filter.go
@@ -1,11 +1,12 @@
 package gslices
 
-func FilterInplace[T any](s []T, f func(x T) bool) []T {
-	keep := s[:0]
-	for _, x := range s {
+func FilterInplace[T any](s *[]T, f func(x T) bool) {
+
+	keep := (*s)[:0]
+	for _, x := range *s {
 		if f(x) {
 			keep = append(keep, x)
 		}
 	}
-	return keep
+	*s = keep
 }
