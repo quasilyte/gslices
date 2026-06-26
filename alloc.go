373a02fc48fa7b0ae5e9d0cfdc1aa8ab7bcb4b12
Iskander Sharipov <quasilyte@gmail.com>
2026-06-27 00:41:24 +0400

    replace realloc with resize

--- alloc.go
+++ alloc.go
@@ -11,40 +11,29 @@ func AllocElem[T any](s *[]T) *T {
 	return &(*s)[len(*s)-1]
 }
 
-// ReallocZeroed forces the slice s to be of the specified size.
+// ResizeZeroed forces the slice s to be of the specified length.
 // If s has enough capacity, the memory will be re-used.
 // It does zeroing on the result.
-func ReallocZeroed[T any](s *[]T, size int) {
+func ResizeZeroed[T any](s *[]T, length int) {
 	tmp := *s
-	switch {
-	case cap(tmp) == 0:
-		tmp = make([]T, size)
-	case cap(tmp) >= size:
-		tmp = tmp[:size]
+	if cap(tmp) >= length {
+		tmp = tmp[:length]
 		clear(tmp)
-	default:
-		// Re-use and zero max amount of memory, and then
-		// append some more to meet the required size.
-		tmp = tmp[:cap(tmp)]
-		clear(tmp)
-		tmp = append(tmp, make([]T, size-cap(tmp))...)
+	} else {
+		tmp = make([]T, length)
 	}
 	*s = tmp
 }
 
-// Realloc is like ReallocZeroed, but does no zeroing.
+// Resize is like ResizeZeroed, but does no zeroing.
 // Use it when you are sure that up to size elements
 // will be overwritten right away (or you don't care about those).
-func Realloc[T any](s *[]T, size int) {
+func Resize[T any](s *[]T, length int) {
 	tmp := *s
-	switch {
-	case cap(tmp) == 0:
-		tmp = make([]T, size)
-	case cap(tmp) >= size:
-		tmp = tmp[:size]
-	default:
-		tmp = tmp[:cap(tmp)]
-		tmp = append(tmp, make([]T, size-cap(tmp))...)
+	if cap(tmp) >= length {
+		tmp = tmp[:length]
+	} else {
+		tmp = make([]T, length)
 	}
 	*s = tmp
 }
